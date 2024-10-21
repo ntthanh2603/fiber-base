415b396c97b01e320828e13e0a97e7680404f16e
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2024-10-21 22:28:20 +0700

    refeactor code

--- backend/src/users/dto/login-user.dto.ts
+++ backend/src/users/dto/login-user.dto.ts
@@ -10,14 +10,14 @@ import {
 export class LoginUserDto {
   @IsNotEmpty({ message: 'Email không được trống' })
   @IsEmail({}, { message: 'Email không đúng định dạng' })
-  @IsString()
+  @IsString({ message: 'Type email is string' })
   @ApiProperty({ example: 'user@gmail.com', description: 'email' })
   email: string;
 
   @IsNotEmpty({ message: 'Mật khẩu không được trống' })
   @MinLength(8, { message: 'Mật khẩu không được dưới 8 kí tự' })
   @MaxLength(10, { message: 'Mật Khẩu không được trên 10 kí tự' })
-  @IsString()
+  @IsString({ message: 'Type password is string' })
   @ApiProperty({ example: '12345678', description: 'password' })
   password: string;
 }
