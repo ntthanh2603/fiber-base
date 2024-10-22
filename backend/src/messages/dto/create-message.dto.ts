f3bd3b9d5a0d3a08c7a052dd9abc513767721427
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2024-10-22 13:39:34 +0700

    feat: messages

--- backend/src/messages/dto/create-message.dto.ts
+++ backend/src/messages/dto/create-message.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateMessageDto {
+  @IsString()
   @IsNotEmpty({ message: 'conversation_id not null' })
   conversation_id: string;
 
+  @IsString()
   @IsNotEmpty({ message: 'message not null' })
   message: string;
 }
--- backend/src/messages/messages.controller.ts
+++ backend/src/messages/messages.controller.ts
@@ -12,11 +12,13 @@ import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { IUser } from 'src/users/users.interface';
 import { ResponseMessage, User } from 'src/decorator/customize';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Messages')
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
-  @Post('/create')
+  @Post()
   @ResponseMessage('Create message')
   create(@User() user: IUser, @Body() createMessageDto: CreateMessageDto) {
     return this.messagesService.create(user, createMessageDto);
