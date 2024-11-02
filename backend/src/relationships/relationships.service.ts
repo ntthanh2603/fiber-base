e109156f7e03702ac26bc453095c8e2bb3a530d5
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2024-11-02 16:23:22 +0700

    feat: unfollow

--- backend/src/relationships/relationships.service.ts
+++ backend/src/relationships/relationships.service.ts
@@ -105,19 +105,59 @@ export class RelationshipsService {
         };
       }
     }
-    throw new BadRequestException();
+    throw new BadRequestException(
+      `${user_id1} followed ${user_id2} or ${user_id1} and ${user_id2} is friend`,
+    );
   }
 
   async unFollow(dto: CreateRelationshipDto, user: IUser) {
-    // const { user_id2 } = updateRelationshipDto;
-    // const user2 = await this.usersService.findUserById(user_id2);
-    // if (!user2) throw new NotFoundException();
-    // const relationship = await this.findRelationship(user.user_id, user_id2);
-    // if (relationship) {
-    //   return await this.relationshipsRepository.delete(relationship);
-    // }
-    // return {
-    //   message: 'UnFollowed',
-    // };
+    const { user_id2 } = dto;
+    const user_id1 = user.user_id;
+
+    const user2 = await this.usersService.findUserById(user_id2);
+
+    if (!user2) throw new NotFoundException();
+
+    const relationship_user1_user2 = await this.findRelationship(
+      user_id1,
+      user_id2,
+    );
+
+    const relationship_user2_user1 = await this.findRelationship(
+      user_id2,
+      user_id1,
+    );
+
+    if (!relationship_user1_user2) {
+      throw new BadRequestException(
+        `${user_id1} and ${user_id2} not friend or follower`,
+      );
+    } else if (
+      relationship_user1_user2.relationship == RelationshipType.FRIEND
+    ) {
+      await this.relationshipsRepository.delete({
+        user_id1: user_id1,
+        user_id2: user_id2,
+      });
+      await this.relationshipsRepository.update(
+        {
+          user_id1: user_id2,
+          user_id2: user_id1,
+        },
+        {
+          relationship: RelationshipType.FOLLOW,
+        },
+      );
+      return {
+        message: `${user_id1} unfollow ${user_id2} and ${user_id2} follow ${user_id1}`,
+      };
+    } else if (
+      relationship_user1_user2.relationship == RelationshipType.FOLLOW
+    ) {
+      await this.relationshipsRepository.delete(relationship_user1_user2);
+      return {
+        message: `${user_id1} unfollowed ${user_id2}`,
+      };
+    }
   }
 }
