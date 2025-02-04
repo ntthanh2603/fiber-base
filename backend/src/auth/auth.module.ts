dfef5204eac5a5ab0c75234ef07b3fab5d3d9f19
Liam-2603 <tuanthanh2kk4@gmail.com>
2025-02-04 22:18:37 +0700

    refactor

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LocalStrategy } from './passpost/local.strategy';
 import { JwtStrategy } from './passpost/jwt.strategy';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [],
   providers: [LocalStrategy, JwtStrategy],
   exports: [],
